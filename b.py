96ae381ef173004aaa17938df0aa932aef1bd8f9
Elena-VP <evp030281@gmail.com>
2024-11-29 18:50:16 +1100

    Update hello_git py. Add for

---
+++ b.py
@@ -0,0 +1,4 @@
+import math
+
+def sum_num(a,b):
+    return a ** 2 + b ** 2 + math.sqrt(a*b)
--- hello_git.py
+++ hello_git.py
@@ -1,2 +1,17 @@
-print('Hello, Git!')
-print('Индексация это важно')
+import os
+import b
+print('Hello, git')
+print("Index - it's important")
+
+a1 = int(input())
+b1 = int(input())
+
+print(b.sum_num(a1,b1))
+
+for num in range(10):
+        print(num,end=' ')
+
+print(f'\nYour current directory: {os.getcwd()}')
+
+s = 'Version Control System'
+print(f'{s} - length is {len(s)} characters')
